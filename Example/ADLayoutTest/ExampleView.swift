6e91e7ec8435490febfe853d14ead6f505900132
Pierre Felgines <pierre.felgines@fabernovel.com>
2019-04-12 10:25:51 +0200

    Create example of random tests
    Change-Id: Ida952bc309a6ba660c13cb0190f7b00e9b1dd8e9

---
+++ Example/ADLayoutTest/ExampleView.swift
@@ -0,0 +1,31 @@
+//
+//  ExampleView.swift
+//  ADLayoutTest_Example
+//
+//  Created by Pierre Felgines on 12/04/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ExampleViewModel {
+    let image: UIImage?
+    let text: String
+    let subText: String
+}
+
+class ExampleView: UIView {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var subLabel: UILabel!
+
+    // MARK: - Public
+
+    func configure(with viewModel: ExampleViewModel) {
+        imageView.image = viewModel.image
+        label.text = viewModel.text
+        subLabel.text = viewModel.subText
+    }
+}
--- Example/ADLayoutTest_ExampleTests/ADLayoutTest_ExampleTests.swift
+++ Example/ADLayoutTest_ExampleTests/ADLayoutTest_ExampleTests.swift
@@ -7,27 +7,91 @@
 //
 
 import XCTest
+import ADLayoutTest
+import SwiftCheck
+@testable import ADLayoutTest_Example
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return fromNib(named: String(describing: T.self))
+    }
+
+    class func fromNib<T: UIView>(named: String) -> T {
+        return Bundle(for: T.self).loadNibNamed(named, owner: nil, options: nil)![0] as! T
+    }
+}
+
+extension ExampleViewModel: Arbitrary {
+
+    public static var arbitrary: Gen<ExampleViewModel> {
+        return Gen<ExampleViewModel>.compose { c in
+            return ExampleViewModel(
+                image: c.generate(using: .image(min: 20, max: 100)),
+                text: c.generate(using: .words),
+                subText: c.generate(using: .words)
+            )
+        }
+    }
+}
 
 class ADLayoutTest_ExampleTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testExampleView() {
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        runLayoutTests(named: "ExampleView") { (viewModel: ExampleViewModel) in
+            let view: ExampleView = ExampleView.fromNib(named: "ExampleViewValid")
+            view.backgroundColor = UIColor.white
+            view.frame = CGRect(x: 0, y: 0, width: 320.0, height: 150.0)
+            view.configure(with: viewModel)
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+            do {
+                try view.ad_runBasicRecursiveTests()
+            } catch {
+                return .failure(view, error)
+            }
+            return .success(view)
         }
     }
 
+    func testExampleViewScreeshots() {
+
+        runLayoutTests(
+            named: "ExampleView Screenshots",
+            snapshotStrategy: .allTests,
+            maxTestsCount: 10
+        ) { (viewModel: ExampleViewModel) in
+            let view: ExampleView = ExampleView.fromNib(named: "ExampleViewValid")
+            view.backgroundColor = UIColor.white
+            view.frame = CGRect(x: 0, y: 0, width: 320.0, height: 150.0)
+            view.configure(with: viewModel)
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+            do {
+                try view.ad_runBasicRecursiveTests()
+            } catch {
+                return .failure(view, error)
+            }
+            return .success(view)
+        }
+    }
+
+    // This test should fail
+    func testExampleViewError() {
+
+        runLayoutTests(named: "ExampleView Overlap") { (viewModel: ExampleViewModel) in
+            let view: ExampleView = ExampleView.fromNib(named: "ExampleViewError")
+            view.backgroundColor = UIColor.white
+            view.frame = CGRect(x: 0, y: 0, width: 320.0, height: 150.0)
+            view.configure(with: viewModel)
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+            do {
+                try view.ad_runBasicRecursiveTests()
+            } catch {
+                return .failure(view, error)
+            }
+            return .success(view)
+        }
+    }
 }
