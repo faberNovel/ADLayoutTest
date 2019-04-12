ceea39a47e620bb72fecda52a8a31c810debfe25
Pierre Felgines <pierre.felgines@fabernovel.com>
2019-04-12 10:38:30 +0200

    Create useful random generators
    Change-Id: I63d4a308d1653d7ac0ce812a03e9b336b1b62583

---
+++ ADLayoutTest/Classes/Gen+Character.swift
@@ -0,0 +1,33 @@
+//
+//  Gen+Character.swift
+//  ADLayoutTest
+//
+//  Created by Pierre Felgines on 11/04/2019.
+//
+
+import Foundation
+import SwiftCheck
+
+extension Gen where A == Character {
+
+    /// Generator of random character between `"A"` and `"Z"`
+    public static let upper = Gen<Character>.fromElements(in: "A"..."Z")
+
+    /// Generator of random character between `"a"` and `"z"`
+    public static let lower = Gen<Character>.fromElements(in: "a"..."z")
+
+    /// Generator of random character between `"0"` and `"9"`
+    public static let numeric = Gen<Character>.fromElements(in: "0"..."9")
+
+    /// Generator of random special character
+    public static let special = Gen<Character>.fromElements(
+        of: [
+            "!", "#", "$", "%", "&", "'", "*", "+", "-", "/", "=", "?", "^", "_", "`", "{", "|", "}", "~", "."
+        ]
+    )
+
+    /// Generator of random character between `"A"` and `"Z"` or between `"0"` and `"9"`
+    public static let hexDigits = Gen<Character>.one(
+        of: [Gen<Character>.fromElements(in: "A"..."F"), numeric]
+    )
+}
