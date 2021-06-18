7d4e7c4f299c425a0ccf65b3df22785ad68464b5
Daren Hayward <darhaywa@synamedia.com>
2021-06-18 18:03:42 +0100

    Updated to use faberNovel/ADAssertLayout

--- Package.swift
+++ Package.swift
@@ -2,20 +2,22 @@
 import PackageDescription
 
 let package = Package(
-	name: "ADLayoutTest",
-	products: [
-		.library(
-			name: "ADLayoutTest",
-			targets: ["ADLayoutTest"]),
-	],
-	dependencies: [
-		.package(url: "https://github.com/emanuelef/ADAssertLayout.git", from: "1.0.3"),
-    		.package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.12.0")
-	],
-	targets: [
-		.target(
-			name: "ADLayoutTest",
-			dependencies: ["ADAssertLayout", "SwiftCheck"],
-      			path: "ADLayoutTest")
-	]
+    name: "ADLayoutTest",
+    products: [
+        .library(
+            name: "ADLayoutTest",
+            targets: ["ADLayoutTest"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/faberNovel/ADAssertLayout.git", from: "1.0.1"),
+        .package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.12.0")
+    ],
+    targets: [
+        .target(
+            name: "ADLayoutTest",
+            dependencies: ["ADAssertLayout", "SwiftCheck"],
+            path: "ADLayoutTest"
+        )
+    ]
 )
